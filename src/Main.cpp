6dc9a5ed0b23bf3c78b12ceafb8e80afa2e1d989
Mathis Sarazin <msarazin04@gmail.com>
2023-09-27 17:42:59 -0400

    SUp sup

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,8 +3,6 @@
 #include "LibRobus.h"
 #include "Constants.hpp"
 
-void PrintEmile();
-
 void setup() 
 {
 
@@ -12,9 +10,5 @@ void setup()
 
 void loop() 
 {
-
-}
-
-void PrintEmile(){
-    printf("Emile Raymond test 3"); 
+    
 }
