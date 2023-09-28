0bbbac7dd0612438ad718a694ed36b47df007ff3
theo <charle.tanguay@gmail.com>
2023-09-28 09:57:29 -0400

    Fonctions pour mettre a jour les moteurs a une vitesse specifique

--- src/Drivebase.hpp
+++ src/Drivebase.hpp
@@ -9,7 +9,19 @@
 
 namespace p28 {
 
+struct Motor {
+    int ID;
+    PID pid;
+    Error error;
+    float speed;
+    int32_t last_ticks;
+    long int last_time_ms;
+};
+
 float ticks_to_dist(uint32_t ticks);
+
+Motor get_motor_speed(Motor motor, float delta_s);
+Motor update_motor_at_speed(Motor motor, float speed, long int time_ms);
 
 } // !p28
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -2,40 +2,10 @@
 #include <Arduino.h>
 #include "LibRobus.h"
 #include "Constants.hpp"
+#include "Drivebase.hpp"
 
-struct PID {
-	float P;
-	float I;
-	float D;
-};
-struct Error {
-	float error { 0.0 };
-	float diff_error { 0.0 };
-	float sum_error { 0.0 };
-};
-
-Error update_error(Error err, float value, float setpoint, float delta_s)
-{
-	Error out;
-	out.error = setpoint - value;
-	out.diff_error = out.error - err.error;
-	out.sum_error = err.sum_error * 0.999 + out.error * delta_s;
-	return out;
-}
-float get(PID const& pid, Error const& error)
-{
-	return pid.P * error.error + pid.D * error.diff_error + pid.I * error.sum_error;
-}
-
-unsigned long prev_time_ms;
-float set_speed = 0.4; // m/s
-Error left_velocity_error;
-Error right_velocity_error;
-
-PID motorPID;
-uint32_t left_prev_ticks = 0;
-uint32_t right_prev_ticks = 0;
-
+p28::Motor motorLeft;
+p28::Motor motorRight;
 
 float ticks_to_dist(uint32_t ticks)
 {
@@ -45,50 +15,15 @@ float ticks_to_dist(uint32_t ticks)
 void setup() 
 {
 	BoardInit();
-	motorPID.P = 1.4;
-	motorPID.I = 35.5555555555556;
-	motorPID.D = 0.03333333333333;
-	motorPID.P = 4;
-	Serial.begin(9600);
-	delay(1000);
-	Serial.println("Begin!\n");
-	prev_time_ms = millis();
-	delay(10);
+	motorLeft.pid = { 1.4, 35.55555, 0.0333333 };
+	motorRight.pid = { 1.4, 35.55555, 0.0333333 };
 }
 
 void loop() 
 {
-	unsigned long time_ms = millis();
-
-	float delta_s = static_cast<float>(time_ms - prev_time_ms) / 1000.0f;
-
-	uint32_t left_current_ticks = ENCODER_Read(LEFT);
-	float left_current_speed = ticks_to_dist(left_current_ticks-left_prev_ticks) / delta_s;
-	left_velocity_error = update_error(left_velocity_error, left_current_speed, set_speed, delta_s);
-
-	float left_motorset = get(motorPID, left_velocity_error);
-
-	uint32_t right_current_ticks = ENCODER_Read(RIGHT);
-	float right_current_speed = ticks_to_dist(right_current_ticks-right_prev_ticks) / delta_s;
-	right_velocity_error = update_error(right_velocity_error, right_current_speed, set_speed, delta_s);
-
-	float right_motorset = get(motorPID, right_velocity_error);
-
-
-	MOTOR_SetSpeed(LEFT, left_motorset);
-	MOTOR_SetSpeed(RIGHT, right_motorset);
-
-	left_prev_ticks = left_current_ticks;
-	right_prev_ticks = right_current_ticks;
-	prev_time_ms = time_ms;
-
-	// Serial.print(time_ms);
-	// Serial.print(", ");
-	// Serial.print(motorset);
-	// Serial.print(", ");
-	// Serial.println(left_velocity_error.error);
-	// Serial.print(", ");
-	// Serial.println(current_ticks);
+	long int millis_time = millis();
+	motorLeft = p28::update_motor_at_speed(motorLeft, 0.5, millis_time);
+	motorRight = p28::update_motor_at_speed(motorLeft, 0.5, millis_time);
 
 	delay(10);
 }
