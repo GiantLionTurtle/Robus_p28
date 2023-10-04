86ef8074bf06f97b8adbebfb8f1f1b9a24e0c78c
theo <charle.tanguay@gmail.com>
2023-10-03 21:37:00 -0400

    Option de changer de PID selon le robot

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,29 +5,35 @@
 #include "Drivebase.hpp"
 #include "Solver.hpp"
 
+#define ROBOT_A
+
 p28::Drivebase driveBase;
 
-void setup() 
+void setup()
 {
 	BoardInit();
 	delay(1000);
 	Serial.println("Begin!");
 
 	driveBase.left.ID = LEFT;
-	driveBase.left.pid = { 1.4, 35.55555, 0.0333333 };
-
 	driveBase.right.ID = RIGHT;
-	driveBase.right.pid = { 1.4, 35.55555, 0.0333333 };
 
+#ifdef ROBOT_A
+	driveBase.left.pid = { 2.8, 53.4, 0.055 };
+	driveBase.right.pid = { 2.8, 53.4, 0.055 };
+#else
+	driveBase.left.pid = { 2.8, 53.4, 0.055 };
+	driveBase.right.pid = { 2.8, 53.4, 0.055 };
+#endif
 	// MOTOR_SetSpeed(RIGHT, 0.5);
 
-	driveBase = p28::turn_left(driveBase);
-	delay(500);
-	driveBase = p28::turn_right(driveBase);
-	delay(500);
-	// driveBase = p28::forward_dist(driveBase, 0.5, 0.2);
+	// driveBase = p28::turn_left(driveBase);
+	// delay(500);
+	// driveBase = p28::turn_right(driveBase);
+	// delay(500);
+	driveBase = p28::forward_dist(driveBase, 2, 0.2);
 
-	driveBase = p28::solve(driveBase);
+	// driveBase = p28::solve(driveBase);
 }
 
 void loop() 
--- src/Solver.cpp
+++ src/Solver.cpp
@@ -68,24 +68,24 @@ void init_legalityMatrix()
 }
 
 Drivebase solve(Drivebase drvb)
-{
-   if(is_legal_move(drvb, FRONT) == Legality::Can_go) {                
-    move_to_square(drvb, FRONT, 1);
-   }
-     else if(is_legal_move(drvb, FRONT) == Legality::Cannot_go) {
-       // direction_until_detect(drvb, LEFT, kdetectionDistance, detection);
-     }   
-            if(is_legal_move(drvb, LEFT) == Legality::Can_go){
-            move_to_square(drvb, LEFT, 1);
-            }
-       // else if                                                    //code temporaire 
-       // else if(is_legal_move(drvb, LEFT) == Legality::Can_go){
-       // move_to_square(drvb, FRONT, 1);
-   }
+{}
+//    if(is_legal_move(drvb, FRONT) == Legality::Can_go) {                
+//     move_to_square(drvb, FRONT, 1);
+//    }
+//      else if(is_legal_move(drvb, FRONT) == Legality::Cannot_go) {
+//        // direction_until_detect(drvb, LEFT, kdetectionDistance, detection);
+//      }   
+//             if(is_legal_move(drvb, LEFT) == Legality::Can_go){
+//             move_to_square(drvb, LEFT, 1);
+//             }
+//        // else if                                                    //code temporaire 
+//        // else if(is_legal_move(drvb, LEFT) == Legality::Can_go){
+//        // move_to_square(drvb, FRONT, 1);
+//    }
            // else if(is_legal_move(drvb, LEFT) == Legality::Cannot_go){
               //  turn_right(drvb);
                 //turn_right(drvb);
-            }
+            // }
                // else if(is_legal_move(drvb, FRONT) == Legality::Can_go){
                    // move_to_square(drvb, FRONT, 1);
                // }
@@ -100,4 +100,4 @@ Drivebase solve(Drivebase drvb)
 
 
 
-//} // !p28
+} // !p28
