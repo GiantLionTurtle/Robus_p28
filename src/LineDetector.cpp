69dd052186076824201b0b3cd3c49f09896d1b1e
Maxime <maxime.lebl@outlook.com>
2023-10-29 13:48:42 -0400

    LineDetector

---
+++ src/LineDetector.cpp
@@ -0,0 +1,37 @@
+
+
+#include "LineDetector.hpp"
+#include "LibRobus.h"
+
+namespace p28 {
+    static int PIN_IR[8] {A0, A1, A2, A3, A4, A5, A6, A7};
+    #define BLACK_TRESH 200
+    #define BASIC_TRESH 900
+
+
+    char get_ir_line()
+    {
+        char byte_line = 0b00000000;
+        for(int i = 0; i < 8; i++)
+        {
+            bool state = get_ir(i);
+             byte_line |= state<<i;
+        }
+        
+        return byte_line;
+    }
+
+    bool get_ir(int index)
+    {
+        bool state = 0;
+        if(index >=0 && index <8)
+        {
+            int irRead = analogRead(PIN_IR[index]);
+            if(irRead >= BLACK_TRESH)
+            {
+                state = 1;
+            }
+        }
+        return state;
+    }
+}
---
+++ src/LineDetector.hpp
@@ -0,0 +1,8 @@
+#ifndef P28_LINEDETECTOR_HPP_
+#define P28_LINEDETECTOR_HPP_
+
+namespace p28{
+    char get_ir_line();
+    bool get_ir(int index);
+} // !p28
+#endif
