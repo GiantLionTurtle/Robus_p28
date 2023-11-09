6b87e911b8b71a23751404c87727e6608a17ed9c
Mathieu Lorincz <mathieulorincz@icloud.com>
2023-11-09 13:53:09 -0500

    clearance of GameState

--- src/Robot.hpp
+++ src/Robot.hpp
@@ -41,6 +41,33 @@ struct Robot {
 								GameState const& prevGmState, GameState const& gmState);
 };
 
-} // !p28
+struct Objective {
+	enum Doneness : char { Todo, Start, UnderWay, Done };
+
+	Doneness donneness;
+	time_t start_ms;
+
+	bool todo() const { return donneness == Doneness::Todo; }
+	bool start() const { return donneness == Doneness::Start; }
+	bool underway() const { return donneness == Doneness::UnderWay; }
+	bool done() const { return donneness == Doneness::Done; }
+
+	Objective advance(bool start_cond, bool end_cond, time_t time_ms) const
+	{
+		if(todo() && start_cond) {
+			return Objective { .donneness=Doneness::Start, .start_ms=time_ms };
+		}
+		if(start()) {
+			return Objective { .donneness=Doneness::UnderWay, .start_ms=start_ms };
+		}
+		if(underway() && end_cond) {
+			return Objective { .donneness=Doneness::Done, .start_ms=start_ms };
+		}
+		return *this;
+	}
+
+}; // !p28
+
+}
 
 #endif
