93c1ddff5bb2c64c4d59c183aad7997287983ea9
Julien Roman <romj1804@usherbrooke.ca>
2023-11-16 10:49:10 -0500

    conveyor sequence done

--- src/Subsystems/conveyor.cpp
+++ src/Subsystems/conveyor.cpp
@@ -13,31 +13,41 @@ namespace p28 {
     struct Step{
         float clawServo;
         float armServo;
-        long delayAction;
+        long stepTime;
 
 
     };
     const int Nsteps = 4;
     Step sequence[Nsteps] = {
-        Step { .clawServo = kClaw_openAngle, .armServo = kArm_openAngle, .delayAction =2000},
-        Step { .clawServo = kClaw_closeAngle, .armServo = kArm_openAngle, .delayAction = 2000},
-        Step { .clawServo = kClaw_closeAngle, .armServo = kArm_closeAngle, .delayAction = 2000},
-        Step { .clawServo = kClaw_openAngle, .armServo = kArm_closeAngle, .delayAction =2000}
+        Step { .clawServo = kClaw_openAngle, .armServo = kArm_openAngle, .stepTime =2000},
+        Step { .clawServo = kClaw_closeAngle, .armServo = kArm_openAngle, .stepTime = 2000},
+        Step { .clawServo = kClaw_closeAngle, .armServo = kArm_closeAngle, .stepTime = 2000},
+        Step { .clawServo = kClaw_openAngle, .armServo = kArm_closeAngle, .stepTime =2000}
         
     };
+    Conveyor Start_ConveyorTime(Iteration_time it_time, Conveyor conveyorState){
+        conveyorState.startStepTime = it_time.time_ms;
+        conveyorState.conveyorSequence = 0;
+        return conveyorState;
+    }
 
 
-
-    HardwareState update_conveyorSequence(Conveyor conveyorState, Step sequence, HardwareState hrdwState,Iteration_time it_time ){
+    HardwareState update_conveyorSequence(Conveyor conveyorState, Step sequence[], HardwareState hrdwState,Iteration_time it_time ){
         
-        // int stepIndex = conveyorState.conveyorSequence;
-        // unsigned long sequenceTime = conveyorState.sequenceTime;
+        int stepIndex = conveyorState.conveyorSequence;
+        if ( stepIndex < 4){
+            hrdwState.armAngle = sequence[stepIndex].armServo;
+            hrdwState.clawAngle = sequence[stepIndex].clawServo;
+            if (it_time.time_ms - conveyorState.startStepTime >= sequence[stepIndex].stepTime){
+                stepIndex++;
+                conveyorState.startStepTime = it_time.time_ms;
+            }
+        }
+        else if (stepIndex == 4){
+            hrdwState.armAngle = sequence[0].armServo;
+            hrdwState.clawAngle = sequence[0].clawServo;
+        }
         
-        
-        // if (stepIndex = -1 & stepIndex < 4){
-        //     if (sequence[stepIndex])
-        // }
-        
-        // return hrdwState;
-     }
+        return hrdwState;
+    }
 }
